7c22eae1a31fa3eed44ffadbd92d53b3fe82f9d9
Luce Carter <luce@lucyscarter.co.uk>
2021-05-01 21:44:57 +0100

    Quick and dirty

---
+++ DadJokeBot/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+using System.Media;
+
+namespace DadJokeBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new System.Threading.Timer(async (e) =>
+            {
+                await WriteDadJokeToFile();
+            }, null, TimeSpan.Zero, TimeSpan.FromSeconds(30));
+           //Task.Run(async () => await WriteDadJokeToFile());
+            Console.ReadLine();
+        }
+
+        private static async Task WriteDadJokeToFile()
+        {
+            var joke = "";
+            using(var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Accept", "text/plain");
+                client.DefaultRequestHeaders.Add("User-Agent", "LuceCarter Stream Bot");
+
+                Console.WriteLine("Fetching Dad Joke...");
+                var dadJoke = await client.GetAsync("https://icanhazdadjoke.com/");
+                joke = dadJoke.Content.ReadAsStringAsync().Result;  
+            }
+            
+            try
+            {
+                await File.WriteAllTextAsync("D:/LuceCarter/joke.txt", joke);
+
+                var soundPlayer = new SoundPlayer();
+                soundPlayer.SoundLocation = "D:/LuceCarter/joke.wav";
+                soundPlayer.Play();
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
